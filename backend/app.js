4162cce9cc18dd8a52bdb4ae527111944d704240
cuongcmnr <134992497+cuongcmnr@users.noreply.github.com>
2024-06-12 00:18:34 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require('express-session');
 const PORT = process.env.PORT || 5002;
 const app = express();
 require("./db");
@@ -8,13 +9,14 @@ const adminRoute = require("./routes/admin");
 const reviewRoute = require("./routes/review");
 const path = require("path");
 /************ MIDDLEVARES *************/
-
-// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Add headers in order to perform all operation on API
-// Because CORS Thing (Google it if you do not know)
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 24 * 60 * 60 * 1000 }
+}));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Header", "*");
@@ -24,8 +26,8 @@ app.use((req, res, next) => {
 /************ ROUTES *************/
 app.use("/api/product", productRoute);
 app.use("/api/user", authRoute);
-app.use("/api/admin", admin);
-app.use("/api/review", review);
+app.use("/api/admin", adminRoute);
+app.use("/api/review", reviewRoute);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../frontend/build"));
 
