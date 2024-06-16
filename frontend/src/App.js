428e7d32d881aeeb7729948fc21dda97e6466ca4
Linh <153449781+tlinh213574@users.noreply.github.com>
2024-06-16 16:33:18 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,54 @@
-//import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar.js";
 import Home from "./pages/Home.js";
-import Menu from "./pages/Menu.js"
-import Admin from "./pages/Admin.js"
-import Login from "./pages/Login.js"
+import Menu from "./pages/Menu.js";
+import Admin from "./pages/Admin.js";
+import Login from "./pages/Login.js";
 import Contact from './pages/Contact.js';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Popup from './components/Popup.js'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Popup from './components/Popup.js';
 import { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 
 function App() {
   const [timedPopup, setTimedPopup] = useState(false);
 
-  useEffect(()=>{
-    const popupClosed = Cookies.get('popupClosed');
-    if (!popupClosed) {
+  useEffect(() => {
+    const popupShown = Cookies.get('popupShown');
+    if (!popupShown) {
       setTimeout(() => {
         setTimedPopup(true);
-      }, 60000);
+      }, 3000);
     }
   }, []);
 
-  const handlePopupClose = () => {
+  const closePopup = () => {
+    Cookies.set('popupShown', 'true', { expires: 7 }); 
     setTimedPopup(false);
-    Cookies.set('popupClosed', 'true', { expires: 1 }); // Expires in 1 day
   };
 
   return (
     <div className="App">
-
-      <Popup trigger={timedPopup} setTrigger={handlePopupClose}>
+      {timedPopup && (
+        <Popup trigger={timedPopup} setTrigger={closePopup}>
           <h3>Xiaomi Mi Comport</h3>
           <p>1.100.000đ</p>
-      </Popup>
+        </Popup>
+      )}
 
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" exact element={<Home/>}/>
-          <Route path="/menu" exact element={<Menu/>}/>
-          <Route path="/admin" exact element={<Admin/>}/>
-          
-          <Route path="/contact" exact element={<Contact/>}/>
-          <Route path="/login" exact element={<Login/>}/>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/menu" exact element={<Menu />} />
+          <Route path="/admin" exact element={<Admin />} />
+          <Route path="/contact" exact element={<Contact />} />
+          <Route path="/login" exact element={<Login />} />
         </Routes>
-        
       </Router>
     </div>
   );
 }
 
 export default App;
+// need npm install js-cookie 
