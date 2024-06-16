61f45f34dfee7f35fd2280b52ad29334cadb8d01
Linh <153449781+tlinh213574@users.noreply.github.com>
2024-06-16 14:18:56 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,36 @@ import Menu from "./pages/Menu.js"
 import Admin from "./pages/Admin.js"
 import Login from "./pages/Login.js"
 import Contact from './pages/Contact.js';
-
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Popup from './components/Popup.js'
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 function App() {
+  const [timedPopup, setTimedPopup] = useState(false);
+
+  useEffect(()=>{
+    const popupClosed = Cookies.get('popupClosed');
+    if (!popupClosed) {
+      setTimeout(() => {
+        setTimedPopup(true);
+      }, 60000);
+    }
+  }, []);
+
+  const handlePopupClose = () => {
+    setTimedPopup(false);
+    Cookies.set('popupClosed', 'true', { expires: 1 }); // Expires in 1 day
+  };
+
   return (
     <div className="App">
+
+      <Popup trigger={timedPopup} setTrigger={handlePopupClose}>
+          <h3>Xiaomi Mi Comport</h3>
+          <p>1.100.000đ</p>
+      </Popup>
+
       <Router>
         <Navbar/>
         <Routes>
