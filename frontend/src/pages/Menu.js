e525728db464b83b76e6d8fdb4caefb9a159d231
cuongcmnr <134992497+cuongcmnr@users.noreply.github.com>
2024-06-16 16:41:51 +0700

    Update Menu.js

--- frontend/src/pages/Menu.js
+++ frontend/src/pages/Menu.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import{MenuList} from '../helpers/MenuList'
 import MenuItem from '../components/MenuItem'
 import '../styles/Menu.css'
 
 function Menu() {
+  useEffect(() => {
+    fetch('http://127.0.0.1:5002/api/admin/views/Menu', { 
+      method: 'POST',
+    })
+      .catch(error => console.error('Error updating views:', error));
+  }, [])
   return (
     <div className='menu'>
-      <h1 className='menuTitle'>Our Product</h1>
+      <h1 className='menuTitle'>Our menu</h1>
       <div children='menuList'>
         {MenuList.map((menuItem, key)=>{
             return <MenuItem
             key={key}
             image={menuItem.image}
             name={menuItem.name}
-            link={menuItem.link}
+            price={menuItem.price}
             />
         })}
 
